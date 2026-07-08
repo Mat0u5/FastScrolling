0d5fb2dd518042127dbeb16ec05df69ef663fb34
Mat0u5 <matous.danko@gmail.com>
2026-07-08 19:16:08 +0200

    Forgor

--- src/main/java/net/mat0u5/fastscrolling/Main.java
+++ src/main/java/net/mat0u5/fastscrolling/Main.java
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.Logger;
 public class Main {
 
 	public static final String MOD_ID = "fastscrolling";
-	public static final String MOD_VERSION = "1.0.0";
+	public static final String MOD_VERSION = "1.1.0";
 	public static final String MOD_FRIENDLY_NAME = "Fast Scrolling";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
